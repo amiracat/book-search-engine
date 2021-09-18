684c8bec0f16a07033c38fadc295d99254d224db
Amira Holden <amiraholden@gmail.com>
2021-09-18 11:53:53 -0500

    tweaks to client-side utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,10 @@
+// make a search to google books api
+// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+export const searchGoogleBooks = (query) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  };
+  
+
 // // route to get logged in user's info (needs the token)
 // export const getMe = (token) => {
 //   return fetch('/api/users/me', {
@@ -50,8 +57,3 @@
 //   });
 // };
 
-// // make a search to google books api
-// // https://www.googleapis.com/books/v1/volumes?q=harry+potter
-// export const searchGoogleBooks = (query) => {
-//   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-// };
