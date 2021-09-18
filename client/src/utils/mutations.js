b0dd79c346b50372665c8186f4e3f4a056ff8964
Amira Holden <amiraholden@gmail.com>
2021-09-18 11:43:07 -0500

    adding mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
-// REVISE Below
+
+
 export const SAVE_BOOK = gql`
   mutation saveBook($body: saveBookInput!) {
     saveBook(body: $body) {
@@ -37,7 +38,28 @@ mutation removeBook($bookId: String!) {
     }
   }
 `;
-// add more here
-export const LOGIN_USER = gql ``;
 
-export const ADD_USER = gql ``;
+export const LOGIN_USER = gql `
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+  `;
+
+
+export const ADD_USER = gql `
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+  `;
