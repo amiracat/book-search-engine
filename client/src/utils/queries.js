1fb7d39316a3ccbb68f87b9f8fc0c72597298a96
Amira Holden <amiraholden@gmail.com>
2021-09-18 11:46:04 -0500

    modified queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,29 +1,50 @@
 import { gql } from '@apollo/client';
-// REVISE BELOW
-export const QUERY_USERS = gql`
-  query user {
-    user {
-      _id
-      username
-      email
-      password
-      savedBooks {
-
-      }
-    }
-  }
-`;
-
-export const QUERY_BOOKS = gql`
-  query book($_id: String) {
-    book(_id: $_id) {
-      _id
-      authors
-      description
+// possibly need to modify query me line 4
+export const QUERY_ME = gql`
+  query me {
+    _id
+    email
+    username
+    savedBooks {
       bookId
+      author
+      description
+      title
       image
       link
-      title
     }
   }
 `;
+
+
+
+
+
+// REVISE BELOW
+// export const QUERY_USERS = gql`
+//   query user {
+//     user {
+//       _id
+//       username
+//       email
+//       password
+//       savedBooks {
+
+//       }
+//     }
+//   }
+// `;
+
+// export const QUERY_BOOKS = gql`
+//   query book($_id: String) {
+//     book(_id: $_id) {
+//       _id
+//       authors
+//       description
+//       bookId
+//       image
+//       link
+//       title
+//     }
+//   }
+// `;
