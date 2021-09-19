52ec7ca028095b64571e2af573ae11bcd481ddea
Amira Holden <amiraholden@gmail.com>
2021-09-19 11:36:45 -0500

    trying to get signup to function

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
 
 import { REMOVE_BOOK } from '../utils/mutations';
@@ -15,7 +15,7 @@ const SavedBooks = () => {
   const userData = data?.me || {};
 
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  // const userDataLength = Object.keys(userData).length;
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
