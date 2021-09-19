f06bda0a1755ed0ed7065b2b2c69f4ee9fa71d1d
Amira Holden <amiraholden@gmail.com>
2021-09-18 22:25:55 -0500

    deleting unnecessary js file

--- client/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { QUERY_MATCHUPS } from '../utils/queries';
-
-const Home = () => {
-  const { loading, data } = useQuery(QUERY_BOOKS, {
-    fetchPolicy: "no-cache"
-  });
-// look at activity 13
