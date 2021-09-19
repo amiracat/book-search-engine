af39aca542080433004b33512b71169b6a21c420
Amira Holden <amiraholden@gmail.com>
2021-09-18 22:24:50 -0500

    modify queries file

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,12 @@
 import { gql } from '@apollo/client';
-// possibly need to modify query me line 4
+
 export const QUERY_ME = gql`
   {
     me {
       _id
       email
       username
+      bookCount
       savedBooks {
         bookId
         author
