aec109eccbfc67e3cdd5c41c6c2cef4aa99f2e57
Amira Holden <amiraholden@gmail.com>
2021-09-18 22:23:29 -0500

    cleaning up mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,45 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+  `;
 
-export const SAVE_BOOK = gql`
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          author
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+  `;
+
+  export const SAVE_BOOK = gql`
   mutation saveBook($bookData: saveBookInput!) {
     saveBook(bookData: $bookData) {
       _id
       username
       email
-      bookCount
       savedBooks {
         bookId
         author
@@ -37,28 +69,3 @@ export const REMOVE_BOOK = gql`
     }
   }
 `;
-
-export const LOGIN_USER = gql `
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-  `;
-
-
-export const ADD_USER = gql `
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-  `;
