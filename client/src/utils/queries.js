c60e57536d878ee9614b4f2cc6d022e59c50554c
Amira Holden <amiraholden@gmail.com>
2021-09-19 20:17:07 -0500

    queries update

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,14 +1,19 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-  query me {
-    users {  
+  query user {
+    me {  
       _id
       username
       email
       bookCount
       savedBooks {
-    
+        bookId
+        authors
+        description
+        image
+        link
+        title
       }
     }
   }
